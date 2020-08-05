38c7a75179e96bb6e0163ed9adf8423b1db7ad99
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-05 18:49:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+let arr=[];
+let x=0;
+function mapToNegativize(arr)
+{
+  return arr.map(n=>n * -1);
+}
+
+function mapToNoChange(arr)
+{
+  return arr.map(n=>n);
+}
+
+function mapToDouble(arr)
+{
+  return arr.map(n=>n *2);
+}
+
+
+function mapToSquare(arr)
+{
+  return arr.map(n=>n *n);
+}
+
+function reduceToTotal(arr)
+{
+const sum= arr.reduce(function (result, item) {  return result + item;});
+  return sum;
+}
+function reduceToTotal(arr,x)
+{
+ const sum= arr.reduce(function (result, item) {  return result + item;},x);
+  return sum ;
+}
+
+
+
+function reduceToAnyTrue(arr,search)
+{
+  return arr.reduce((items, value) => items && Boolean(search(value)), true);
+
+}
